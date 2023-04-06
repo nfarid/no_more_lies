3ad5c3d9b757734e64113971dc42e26d70414496
nfarid <54642193+nfarid@users.noreply.github.com>
2023-04-06 18:31:49 +0100

    Create backend

---
+++ backend/index.ts
@@ -0,0 +1,15 @@
+
+import express from "express";
+
+const app = express();
+
+
+app.get("/", (req, res)=>{
+    res.send("Hello from express and typescript\n");
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=>{
+    console.log(`App listening on PORT ${port}`);
+});
