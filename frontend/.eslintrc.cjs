5546defdd488997eba126d82d1e4a47cd60d3bec
nfarid <54642193+nfarid@users.noreply.github.com>
2023-04-06 18:40:20 +0100

    Edited frontend eslint

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,10 +1,11 @@
-export default {
+module.exports = {
     "env": {
         "browser": true,
         "es2021": true
     },
     "extends": [
         "eslint:recommended",
+        "plugin:react/recommended",
         "plugin:@typescript-eslint/recommended"
     ],
     "overrides": [
@@ -15,6 +16,7 @@ export default {
         "sourceType": "module"
     },
     "plugins": [
+        "react",
         "@typescript-eslint"
     ],
     "rules": {
