21473b64cbdc51b80bd857951c27e97d8772a214
nfarid <54642193+nfarid@users.noreply.github.com>
2023-04-09 18:39:42 +0100

    Add db/pool.ts

---
+++ backend/src/db/pool.ts
@@ -0,0 +1,21 @@
+
+
+import pg from "pg";
+const { Pool } = pg;
+
+console.log("process.env.PG_USER");
+console.log(process.env.PG_USER);
+const dbPool = new Pool({
+    host: process.env.PG_HOST,
+    port: parseInt(process.env.PG_PORT || ""),
+    database: process.env.PG_DATABASE,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+});
+
+//Do not use pool.query if you are using a transaction.
+//You must use the same client instance for all statements within a transaction.
+
+//Column names cannot be parameterised.
+
+export default dbPool;
