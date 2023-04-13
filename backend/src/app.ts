60af4e413b35aa8e8dcba57f01d1914175f32e70
nfarid <54642193+nfarid@users.noreply.github.com>
2023-04-13 12:19:16 +0100

    Remove user login

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,33 +4,8 @@ import "dotenv/config";
 import express, {Request, Response, NextFunction} from "express";
 
 import indexRoute from "./routes/index.js";
-import sessionRoute from "./routes/session.js";
-import userRouter from "./routes/user.js";
-
-import expressSession from "express-session";
-import connectPgSimple from "connect-pg-simple";
-import pool from "./db/pool.js";
 
 const app = express();
-
-const oneDay = 24 * 60 * 60 * 1000; //a day in milliseconds
-const PostgresqlStore = connectPgSimple(expressSession);
-const sessionStore = new PostgresqlStore({pool});
-//TODO: Change these options
-const sessionOptions : expressSession.SessionOptions = {
-    secret: process.env.SESSION_SECRET || "",
-    cookie: {
-        path: "/",
-        httpOnly: true,
-        secure: false,
-        maxAge: oneDay,
-    },
-    resave: false,
-    saveUninitialized: false,
-    store: sessionStore,
-};
-const session = expressSession(sessionOptions);
-app.use(session);
 
 
 //req.body will be empty without this
@@ -38,10 +13,8 @@ app.use(express.json() );
 app.use(express.urlencoded({extended: true}) );
 
 app.use("/_api", indexRoute);
-app.use("/_api/users", userRouter);
-app.use("/_api/sessions", sessionRoute);
 
-app.use( (err: Error, req : Request, res : Response, _: NextFunction) => {
+app.use( (err: Error, _req : Request, res : Response, _next: NextFunction) => {
     console.error(err.stack);
     res.status(500).send("Something broke!"); //error 500: Internal Server Error
 });
