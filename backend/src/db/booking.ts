51f41343183e98965a926309439c502badf8ee3b
nfarid <54642193+nfarid@users.noreply.github.com>
2023-04-15 16:45:43 +0100

    Created booking

--- backend/src/db/booking.ts
+++ backend/src/db/booking.ts
@@ -5,3 +5,24 @@ export async function getAllAvailables() : Promise<Date[]> {
     const result = await dbPool.query("SELECT slot FROM Availables ORDER BY slot ASC;");
     return result.rows;
 }
+
+//TODO: Improve this
+export async function bookSlot(slot: Date, email: string) {
+    const dbClient =  await dbPool.connect();
+    try {
+        await dbClient.query("BEGIN");
+        const insertResultPromise =  dbClient.query("INSERT INTO Reservations (slot,email) VALUES ($1, $2);", [slot, email]);
+        const deleteResult = await dbClient.query("DELETE FROM Availables WHERE slot=$1;", [slot]);
+        if(deleteResult.rowCount !== 1) {
+            const err = new Error("Booking slot no longer exist");
+            err.name = "DeleteError";
+            throw err;
+        }
+        await insertResultPromise;
+        await dbClient.query("COMMIT");
+    } catch(err: any) {
+        console.log("ROLLBACK");
+        dbClient.query("ROLLBACK");
+        throw err;
+    }
+}
--- backend/src/routes/booking.ts
+++ backend/src/routes/booking.ts
@@ -1,6 +1,6 @@
 
 import express from "express";
-import {getAllAvailables} from "../db/booking.js";
+import {getAllAvailables, bookSlot} from "../db/booking.js";
 
 const bookingRoute = express.Router();
 
@@ -10,6 +10,23 @@ bookingRoute.get("/", (_, res)=> {
         .then( slotLst=>{
             res.json(slotLst);
         }).catch( (err : Error) =>{
+            console.error(err);
+            res.status(500).send("Internal Server Error"); //http 500: Internal Server Error
+        });
+});
+
+bookingRoute.post("/", (req, res)=> {
+    if(!req.body["booking-email"] || !req.body["booking-slot"])
+        return res.status(400).send("Need email and slot"); //http 400: Bad Request
+    const email : string = req.body["booking-email"];
+    const slot = new Date(req.body["booking-slot"]);
+    bookSlot(slot, email)
+        .then( ()=>{
+            console.log("Commited result");
+            res.status(201).send("Booked slot!");
+        }).catch( (err: Error)=>{
+            if(err.name == "DeleteError")
+                res.status(404).send("Booking slot no longer exist"); //http 404: Not Found
             console.error(err);
             res.status(500).send("Internal Server Error"); //http 500: Internal Server Error
         });
