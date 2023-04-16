ff3ca481990c6eff4c6337cc5fc4539b863c8e2e
nfarid <54642193+nfarid@users.noreply.github.com>
2023-04-16 16:42:04 +0100

    Changed how available slots are returned

--- backend/src/db/booking.ts
+++ backend/src/db/booking.ts
@@ -1,28 +1,59 @@
 
 import dbPool from "./pool.js";
 
-export async function getAllAvailables() : Promise<Date[]> {
-    const result = await dbPool.query("SELECT slot FROM Availables ORDER BY slot ASC;");
-    return result.rows;
+import type { v4 } from "uuid";
+//Default postgres uuid type is v4: https://www.postgresql.org/docs/current/functions-uuid.html
+
+
+export type Availables = {monthYear: Date, days: {date: Date, times: {id: typeof v4, slot: Date}[] }[] }[];
+
+export async function getAllAvailables() : Promise<Availables> {
+    const result = await dbPool.query("SELECT id, slot FROM Availables ORDER BY slot ASC;");
+    const rows : {id: typeof v4, slot: Date}[] = result.rows;
+
+    //Turn the flat result into a hierarchical structure, grouped by dates
+    const ret : Availables =[];
+    for(const row of rows) {
+        const month = row.slot.getMonth();
+        const year = row.slot.getFullYear();
+        const monthYear = new Date(year, month);
+        let lastMonth = ret.slice(-1)[0];
+        if(!lastMonth || lastMonth.monthYear.getTime() !== monthYear.getTime() ) {
+            ret.push({monthYear: monthYear, days: []});
+            lastMonth = ret.slice(-1)[0];
+        }
+        
+        const day = row.slot.getDate();
+        let lastDay = lastMonth.days.slice(-1)[0];
+        if(!lastDay || lastDay.date.getDate() !== day) {
+            lastMonth.days.push({date: new Date(year, month, day), times: []});
+            lastDay = lastMonth.days.slice(-1)[0];
+        }
+
+        lastDay.times.push({id: row.id, slot: row.slot});
+    }
+
+
+    return ret;
 }
 
 //TODO: Improve this
 export async function bookSlot(slot: Date, email: string) {
-    const dbClient =  await dbPool.connect();
-    try {
-        await dbClient.query("BEGIN");
-        const insertResultPromise =  dbClient.query("INSERT INTO Reservations (slot,email) VALUES ($1, $2);", [slot, email]);
-        const deleteResult = await dbClient.query("DELETE FROM Availables WHERE slot=$1;", [slot]);
-        if(deleteResult.rowCount !== 1) {
-            const err = new Error("Booking slot no longer exist");
-            err.name = "DeleteError";
-            throw err;
-        }
-        await insertResultPromise;
-        await dbClient.query("COMMIT");
-    } catch(err: any) {
-        console.log("ROLLBACK");
-        dbClient.query("ROLLBACK");
-        throw err;
-    }
+    // const dbClient =  await dbPool.connect();
+    // try {
+    //     await dbClient.query("BEGIN");
+    //     const insertResultPromise =  dbClient.query("INSERT INTO Reservations (slot,email) VALUES ($1, $2);", [slot, email]);
+    //     const deleteResult = await dbClient.query("DELETE FROM Availables WHERE slot=$1;", [slot]);
+    //     if(deleteResult.rowCount !== 1) {
+    //         const err = new Error("Booking slot no longer exist");
+    //         err.name = "DeleteError";
+    //         throw err;
+    //     }
+    //     await insertResultPromise;
+    //     await dbClient.query("COMMIT");
+    // } catch(err: any) {
+    //     console.log("ROLLBACK");
+    //     dbClient.query("ROLLBACK");
+    //     throw err;
+    // }
 }
--- backend/src/routes/booking.ts
+++ backend/src/routes/booking.ts
@@ -16,20 +16,20 @@ bookingRoute.get("/", (_, res)=> {
 });
 
 bookingRoute.post("/", (req, res)=> {
-    if(!req.body["booking-email"] || !req.body["booking-slot"])
-        return res.status(400).send("Need email and slot"); //http 400: Bad Request
-    const email : string = req.body["booking-email"];
-    const slot = new Date(req.body["booking-slot"]);
-    bookSlot(slot, email)
-        .then( ()=>{
-            console.log("Commited result");
-            res.status(201).send("Booked slot!");
-        }).catch( (err: Error)=>{
-            if(err.name == "DeleteError")
-                res.status(404).send("Booking slot no longer exist"); //http 404: Not Found
-            console.error(err);
-            res.status(500).send("Internal Server Error"); //http 500: Internal Server Error
-        });
+    // if(!req.body["booking-email"] || !req.body["booking-slot"])
+    //     return res.status(400).send("Need email and slot"); //http 400: Bad Request
+    // const email : string = req.body["booking-email"];
+    // const slot = new Date(req.body["booking-slot"]);
+    // bookSlot(slot, email)
+    //     .then( ()=>{
+    //         console.log("Commited result");
+    //         res.status(201).send("Booked slot!");
+    //     }).catch( (err: Error)=>{
+    //         if(err.name == "DeleteError")
+    //             res.status(404).send("Booking slot no longer exist"); //http 404: Not Found
+    //         console.error(err);
+    //         res.status(500).send("Internal Server Error"); //http 500: Internal Server Error
+    //     });
 });
 
 export default bookingRoute;
